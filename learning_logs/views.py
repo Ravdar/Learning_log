f9a6a55c908f78f8f9345c40ef5e72f6d257294e
Ravdar <97836782+Ravdar@users.noreply.github.com>
2023-03-31 19:36:31 +0200

    fsdgs

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -11,25 +11,24 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
-# @login_required
+@login_required
 def topics(request):
-    # topics = Topic.objects.filter(owner=request.user).order_by("date_added")
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
 
 
-# @login_required
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
-    # if topic.owner != request.user:
-    # raise Http404
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
 
 
-# @login_required
+@login_required
 def new_topic(request):
     if request.method != "POST":
         form = TopicForm()
@@ -44,7 +43,7 @@ def new_topic(request):
     return render(request, "learning_logs/new_topic.html", context)
 
 
-# @login_required
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != "POST":
@@ -60,7 +59,7 @@ def new_entry(request, topic_id):
     return render(request, "learning_logs/new_entry.html", context)
 
 
-# @login_required
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
