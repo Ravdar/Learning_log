f060fbfd0b9a6e0320557f178419c69d808e8500
Ravdar <97836782+Ravdar@users.noreply.github.com>
2023-04-16 11:00:01 +0200

    Added own site errors 404 and 500

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s2e&@i+^l71(q333kfya@_$h1sl-7l&e76o%rsfd_tf27z5&7u"
 
 # # SECURITY WARNING: don't run with debug turned on in production!
+# TUTAJ TKWI BŁĄD, NIE CHCE SPUSHOWAC DO HEROKU MASTERA
 # import django_heroku
 # django_heroku.settings(locals())
 # if os.environ.get("DEBUG") == "TRUE":
@@ -67,7 +68,7 @@ ROOT_URLCONF = "learning_log.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
